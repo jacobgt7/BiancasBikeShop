3905c0e489f65847abf00f9e0a0b6d3890fa8480
Jacob Thomas <jacgretho@gmail.com>
2023-01-30 10:17:49 -0600

    bikes list now displays

--- BiancasBikeShop/Repositories/BikeRepository.cs
+++ BiancasBikeShop/Repositories/BikeRepository.cs
@@ -18,7 +18,37 @@ namespace BiancasBikeShop.Repositories
         public List<Bike> GetAllBikes()
         {
             var bikes = new List<Bike>();
-            // implement code here... 
+            
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT b.Id, b.Brand, b.Color,
+                                               o.Id AS OwnerId, o.Name                                              
+                                        FROM Bike b
+                                        JOIN Owner o ON b.OwnerId = o.Id";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var bike = new Bike
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Brand = reader.GetString(reader.GetOrdinal("Brand")),
+                                Color = reader.GetString(reader.GetOrdinal("Color")),
+                                Owner = new Owner
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                                    Name = reader.GetString(reader.GetOrdinal("Name"))
+                                }
+                            };
+                            bikes.Add(bike);
+                        }
+                    }
+                }
+            }
             return bikes;
         }
 
--- BiancasBikeShop/client/src/components/BikeList.js
+++ BiancasBikeShop/client/src/components/BikeList.js
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react"
+import { getBikes } from "../bikeManager"
 import BikeCard from "./BikeCard"
 
-export default function BikeList({setDetailsBikeId}) {
+export default function BikeList({ setDetailsBikeId }) {
     const [bikes, setBikes] = useState([])
 
     const getAllBikes = () => {
-        //implement functionality here...
+        getBikes()
+            .then(bikesData => {
+                setBikes(bikesData)
+            })
     }
 
     useEffect(() => {
@@ -13,8 +17,8 @@ export default function BikeList({setDetailsBikeId}) {
     }, [])
     return (
         <>
-        <h2>Bikes</h2>
-        {/* Use BikeCard component here to list bikes...*/}
+            <h2>Bikes</h2>
+            {bikes.map(bike => <BikeCard key={bike.id} bike={bike} />)}
         </>
     )
 }
